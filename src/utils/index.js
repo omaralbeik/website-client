7196c8c3762421daa93f088ae20a8e83008af11c
Omar Albeik <omaralbeik@gmail.com>
2019-07-13 02:32:46 +0300

    feat: Add copy button to snippet cell

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,3 +17,20 @@ export function objectFromArray(arr, key = 'id') {
 export function arrayFromObject(obj, key = 'id') {
   return Object.keys(obj).map(key => (obj[key]));
 }
+
+export function copyToClipboard(str) {
+  // Create new element
+  var el = document.createElement('textarea');
+  // Set value (string to be copied)
+  el.value = str;
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute('readonly', '');
+  el.style = {position: 'absolute', left: '-9999px'};
+  document.body.appendChild(el);
+  // Select text inside element
+  el.select();
+  // Copy text to clipboard
+  document.execCommand('copy');
+  // Remove temporary element
+  document.body.removeChild(el);
+}
