7e473bf8d0095cba3fe458d45fce86d72b0094e0
Omar Albeik <omar@backbase.com>
2019-12-25 21:05:58 +0100

    fix: fix webpack config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 const withOffline = require('next-offline');
 
+const webpack = require('webpack');
 const { parsed: localEnv } = require('dotenv').config();
 
 const nextConfig = {
