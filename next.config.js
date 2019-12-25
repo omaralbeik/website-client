5d1b4d76f25553b065fb3eb7543dd4d984ba0394
Omar Albeik <omar@backbase.com>
2019-12-25 21:07:40 +0100

    refactor: update webpack config

--- next.config.js
+++ next.config.js
@@ -1,48 +1,17 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 const withOffline = require('next-offline');
-
 const webpack = require('webpack');
 const { parsed: localEnv } = require('dotenv').config();
 
-const nextConfig = {
-  target: 'serverless',
-  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-  workboxOpts: {
-    cleanupOutdatedCaches: true,
-    swDest: '/sw.js',
-    generateInDevMode: true,
-    runtimeCaching: [
-      {
-        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'images',
-          expiration: {
-            maxEntries: 50
-          }
-        }
-      },
-      {
-        urlPattern: /^https?.*api.omaralbeik.com/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'api-calls',
-          expiration: {
-            maxEntries: 50,
-            maxAgeSeconds: 24 * 60 * 60,
-            purgeOnQuotaError: true
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin(localEnv)
-  ]
-};
+const config = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    return config
+  }
+}
 
-const hasOffline = withOffline(nextConfig);
+const hasOffline = withOffline(config);
 const hasImages = withImages(hasOffline);
 const hasCSS = withCSS(hasImages)
 
